bd7e9ef7038137fe4a59ca2d7140436850e72784
204G1A0529 <94513738+204G1A0529@users.noreply.github.com>
2022-09-06 11:38:38 +0530

    S DIYA FARNAAZ created FCFS.c

---
+++ oslab/FCFS.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: S.DIYA FARNAAZ
+   Roll No: 204G1A0529
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 06-09-2022
+   Date of Execution: 06-09-2022
+*/
