180ffb37cffab024c92eb398418a7181fc342437
204G1A0529 <94513738+204G1A0529@users.noreply.github.com>
2022-10-11 11:10:15 +0530

    S.Diya Farnaaz Created SJF.c

---
+++ oslab/SJF.C
@@ -0,0 +1,7 @@
+/* Name Of the Candidate: S.Diya Farnaaz
+   Roll No: 204G1A0529
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 11-10-2022
+   Date of Execution: 11-10-2022
+   */
+  
